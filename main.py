5cb53d2a5f6fc6621897a67a600a6ec1b1c06d6a
RipperDeath <jacklinmelo@protonmail.com>
2023-03-06 13:52:43 -0500

    Added CMD args & and fixed path issue

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-import os
-import sys
 import spotipy
 import argparse
 from config import * #CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 from spotipy.oauth2 import SpotifyOAuth
 from libraryRework import XmlLibraryParser
-
-#detect if os is windows or mac
-if sys.platform == 'win32':
-    try:
-        path = os.environ['USERPROFILE'] + '\\Music\\iTunes\\Library.xml'
-    except:
-        print('iTunes library not found, please make sure You export your library to xml')
-        sys.exit()
-elif sys.platform == 'darwin':
-    try:
-        path = os.environ['HOME'] + '/documents/Library.xml'
-    except:
-        print('iTunes library not found, please make sure You export your library to xml')
-        sys.exit()
-else:
-    print('OS not supported')
-    sys.exit()
 
 #spotify api credentials
 scope = "user-library-modify playlist-modify-private playlist-modify-public"
@@ -36,14 +17,15 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
 parser = argparse.ArgumentParser(description='Convert iTunes library to Spotify')
 parser.add_argument('-l', '--library', action='store_true', help='Add all songs in library to spotify')
 parser.add_argument('-p', '--playlist', action='store_true', help='Add all playlists to spotify')
+parser.add_argument('-lp', '--BOTH', action='store_true', help='Add all songs in library and playlists to spotify')
 args = parser.parse_args()
 
 class SpotifyLibraryConvert:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self):
+        pass
    
     def addToLibrary(self):
-        data = XmlLibraryParser(self.path).libraryToJson()
+        data = XmlLibraryParser().libraryToJson()
         songIdList = []
         for songArtist in data:
             song = songArtist['song']
@@ -54,11 +36,12 @@ class SpotifyLibraryConvert:
             if len(items) > 0:
                 track = items[0]
                 songIdList.append(track['id'])
+                #print('adding track to list')
         sp.current_user_saved_tracks_add(tracks=[songIdList])
         #print('Added ' + song + ' by ' + artist + ' to spotify')
 
     def makePlaylist(self):
-        data = XmlLibraryParser(self.path).playlistToJson()
+        data = XmlLibraryParser().playlistToJson()
         #create playlist and add songs to it
         for playlist in data['playlists']:
             playlistId = sp.user_playlist_create(user=sp.current_user()['id'], name=playlist)['id']
@@ -75,8 +58,13 @@ class SpotifyLibraryConvert:
             sp.user_playlist_add_tracks(user=sp.current_user()['id'], playlist_id=playlistId, tracks=songIdList)
 
 if __name__ == '__main__':
-    convert = SpotifyLibraryConvert(path)
-    
-    convert.addToLibrary()
-    convert.makePlaylist()
+    convert = SpotifyLibraryConvert()
+    #get cmd line args
+    if args.library:
+        convert.addToLibrary()
+    if args.playlist:
+        convert.makePlaylist()
+    if args.BOTH:
+        convert.addToLibrary()
+        convert.makePlaylist()
         
